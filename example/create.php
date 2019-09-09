d5c1fc0ce37010c7031051cabe18dde67d3c54ce
HHarek <hharek@yandex.ru>
2019-09-09 19:43:09 +0300

    CREATE. Примеры изменены

--- example/create.php
+++ example/create.php
@@ -4,6 +4,9 @@ require "settings.php";
 require "table/category.php";
 require "table/product.php";
 
+$db = pg_connect("host=" . DB_HOST . " port=" . DB_PORT . " dbname=" . DB_NAME . " user=" . DB_USER . " password=" . DB_PASSWORD);
+\TM\Table::db_conn($db);
+
 Category::create(true);
 Product::create(true);
 ?>
